2d3b73bffa9e03638282a6d2e8796c7d78e71e7f
PC <fernandezfrancoa2001@gmail.com>
2022-10-05 01:24:49 -0300

    rutas hijas

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RegistroComponent } from './pages/registro/registro.component';
+import { LoginComponent } from './pages/login/login.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+      {
+        path: 'registro',
+        component: RegistroComponent,
+      },
+      {
+        path: '**',
+        redirectTo: 'login',
+      },
+    ],
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class AuthRoutingModule {}

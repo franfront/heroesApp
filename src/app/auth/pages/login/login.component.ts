2eacbf82efa59900c9fa73a8d106d3a50bf22ee4
PC <fernandezfrancoa2001@gmail.com>
2022-10-18 22:27:38 -0300

    empezando a proteger rutas

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../../services/auth.service';
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
@@ -7,9 +8,18 @@ import { Component } from '@angular/core';
   styles: [],
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private authService: AuthService) {}
 
   login() {
-    this.router.navigate(['/heroes']);
+    
+    this.authService.login().subscribe(resp=>{
+      console.log(resp)
+
+      if(resp.id){
+        this.router.navigate(['/heroes']);
+      }
+    })
+    
+
   }
 }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,9 +1,31 @@
+
+import { environment } from './../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Auth } from '../interfaces/auth.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+  private baseUrl: string = environment.baseUrl;
 
-  constructor() { }
+  private authUser: Auth | undefined;
+
+  get auth(){
+    return {...this.authUser}
+  }
+
+
+
+  constructor(private http: HttpClient) { }
+
+  login(){
+
+    return this.http.get<Auth>(`${this.baseUrl}/usuarios/1`).pipe(tap(auth => this.authUser = auth))
+  }
+
+
+
 }
